62c027441eff9c40113ccbc179a764939ee838de
tursunalikhorunaliyev <shahzodbekkomilov2005@gmail.com>
2023-12-07 16:39:32 +0500

    ok

--- lib/view/screen/send_code/screen/send_code_screen.dart
+++ lib/view/screen/send_code/screen/send_code_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class SendCodeScreen extends StatelessWidget {
   static const String routeName = "/send_code_screen";
+  static const String routeName1 = "/send_code_screen";
+  static const String routeName2 = "/send_code_screen";
+
   const SendCodeScreen({super.key});
 
   @override
