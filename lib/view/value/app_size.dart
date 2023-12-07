77277b2a1b9de7e07866d29804fbbb88e653bf29
tursunalikhorunaliyev <shahzodbekkomilov2005@gmail.com>
2023-12-07 14:18:44 +0500

    assaomuasadwvavd

---
+++ lib/view/value/app_size.dart
@@ -0,0 +1,4 @@
+class AppSize {
+  AppSize._();
+  
+}
---
+++ lib/view/value/typography.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Typography {
+  Typography._();
+
+  static const h1 = TextStyle(
+      fontSize: 28,
+      height: 36,
+      fontWeight: FontWeight.w700,
+      fontFamily: "Manrope",
+      color: Colors.black);
+  static const h2 = TextStyle(
+      fontSize: 24,
+      height: 28,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Manrope",
+      color: Colors.black);
+  static const h3 = TextStyle(
+      fontSize: 20,
+      height: 24,
+      fontWeight: FontWeight.w700,
+      fontFamily: "Manrope",
+      color: Colors.black);
+  static const h4 = TextStyle(
+      fontSize: 18,
+      height: 21,
+      fontWeight: FontWeight.w700,
+      fontFamily: "Manrope",
+      color: Colors.black);
+
+  //////////////////////////////////////
+  static const body18Regular = TextStyle(
+      fontSize: 18,
+      height: 21,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Manrope",
+      color: Colors.black);
+  static const body16Regular = TextStyle(
+      fontSize: 16,
+      height: 20,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Manrope",
+      color: Colors.black);
+  static const label = TextStyle(
+      fontSize: 14,
+      height: 18,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Manrope",
+      color: Colors.black);
+  static const body12Regular = TextStyle(
+      fontSize: 12,
+      height: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Manrope",
+      color: Colors.black);
+}
