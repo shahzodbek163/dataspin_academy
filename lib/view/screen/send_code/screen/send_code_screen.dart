e61094a9d4e8af14775727550f07ba7d29b9029d
Usmonaliyev <uzayyub2007@gmail.com>
2023-12-07 16:38:54 +0500

    besbfheabfu

--- lib/view/screen/send_code/screen/send_code_screen.dart
+++ lib/view/screen/send_code/screen/send_code_screen.dart
@@ -6,6 +6,8 @@ class SendCodeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(child: SizedBox()),
+    );
   }
 }
