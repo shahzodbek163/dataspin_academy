eb42861e296c612e04358a73d3a32ee38daf3983
shahzodbek163 <shahzodbek163w@gmail.com>
2023-12-10 12:12:57 +0500

    Der

---
+++ lib/controller/service/api/api_service.dart
@@ -0,0 +1,14 @@
+// import 'package:dio/dio.dart';
+// import 'package:retrofit/http.dart';
+
+// part 'api_service.g.dart';
+
+// @RestApi(baseUrl: "192.168.2.66:8080/api/")
+// abstract class ApiService {
+//   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+//   @POST("auth/send-code")
+//   Future<>sendCode(@Part()String phone);
+
+
+// }
