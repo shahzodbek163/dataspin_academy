1a061febbd03f97e95390f18827ea415dd61033b
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2024-01-22 16:30:11 +0500

    invalid header field name error occured in register user

---
+++ lib/controller/bloc/auth/register/user_register_cubit.freezed.dart
@@ -0,0 +1,543 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'user_register_cubit.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$UserRegisterState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() registering,
+    required TResult Function() registered,
+    required TResult Function() errorOccured,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? registering,
+    TResult? Function()? registered,
+    TResult? Function()? errorOccured,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? registering,
+    TResult Function()? registered,
+    TResult Function()? errorOccured,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_Registering value) registering,
+    required TResult Function(_Registered value) registered,
+    required TResult Function(_Error value) errorOccured,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_Registering value)? registering,
+    TResult? Function(_Registered value)? registered,
+    TResult? Function(_Error value)? errorOccured,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_Registering value)? registering,
+    TResult Function(_Registered value)? registered,
+    TResult Function(_Error value)? errorOccured,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserRegisterStateCopyWith<$Res> {
+  factory $UserRegisterStateCopyWith(
+          UserRegisterState value, $Res Function(UserRegisterState) then) =
+      _$UserRegisterStateCopyWithImpl<$Res, UserRegisterState>;
+}
+
+/// @nodoc
+class _$UserRegisterStateCopyWithImpl<$Res, $Val extends UserRegisterState>
+    implements $UserRegisterStateCopyWith<$Res> {
+  _$UserRegisterStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$InitialImplCopyWith<$Res> {
+  factory _$$InitialImplCopyWith(
+          _$InitialImpl value, $Res Function(_$InitialImpl) then) =
+      __$$InitialImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InitialImplCopyWithImpl<$Res>
+    extends _$UserRegisterStateCopyWithImpl<$Res, _$InitialImpl>
+    implements _$$InitialImplCopyWith<$Res> {
+  __$$InitialImplCopyWithImpl(
+      _$InitialImpl _value, $Res Function(_$InitialImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$InitialImpl implements _Initial {
+  const _$InitialImpl();
+
+  @override
+  String toString() {
+    return 'UserRegisterState.initial()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$InitialImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() registering,
+    required TResult Function() registered,
+    required TResult Function() errorOccured,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? registering,
+    TResult? Function()? registered,
+    TResult? Function()? errorOccured,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? registering,
+    TResult Function()? registered,
+    TResult Function()? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_Registering value) registering,
+    required TResult Function(_Registered value) registered,
+    required TResult Function(_Error value) errorOccured,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_Registering value)? registering,
+    TResult? Function(_Registered value)? registered,
+    TResult? Function(_Error value)? errorOccured,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_Registering value)? registering,
+    TResult Function(_Registered value)? registered,
+    TResult Function(_Error value)? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Initial implements UserRegisterState {
+  const factory _Initial() = _$InitialImpl;
+}
+
+/// @nodoc
+abstract class _$$RegisteringImplCopyWith<$Res> {
+  factory _$$RegisteringImplCopyWith(
+          _$RegisteringImpl value, $Res Function(_$RegisteringImpl) then) =
+      __$$RegisteringImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$RegisteringImplCopyWithImpl<$Res>
+    extends _$UserRegisterStateCopyWithImpl<$Res, _$RegisteringImpl>
+    implements _$$RegisteringImplCopyWith<$Res> {
+  __$$RegisteringImplCopyWithImpl(
+      _$RegisteringImpl _value, $Res Function(_$RegisteringImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$RegisteringImpl implements _Registering {
+  const _$RegisteringImpl();
+
+  @override
+  String toString() {
+    return 'UserRegisterState.registering()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$RegisteringImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() registering,
+    required TResult Function() registered,
+    required TResult Function() errorOccured,
+  }) {
+    return registering();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? registering,
+    TResult? Function()? registered,
+    TResult? Function()? errorOccured,
+  }) {
+    return registering?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? registering,
+    TResult Function()? registered,
+    TResult Function()? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (registering != null) {
+      return registering();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_Registering value) registering,
+    required TResult Function(_Registered value) registered,
+    required TResult Function(_Error value) errorOccured,
+  }) {
+    return registering(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_Registering value)? registering,
+    TResult? Function(_Registered value)? registered,
+    TResult? Function(_Error value)? errorOccured,
+  }) {
+    return registering?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_Registering value)? registering,
+    TResult Function(_Registered value)? registered,
+    TResult Function(_Error value)? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (registering != null) {
+      return registering(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Registering implements UserRegisterState {
+  const factory _Registering() = _$RegisteringImpl;
+}
+
+/// @nodoc
+abstract class _$$RegisteredImplCopyWith<$Res> {
+  factory _$$RegisteredImplCopyWith(
+          _$RegisteredImpl value, $Res Function(_$RegisteredImpl) then) =
+      __$$RegisteredImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$RegisteredImplCopyWithImpl<$Res>
+    extends _$UserRegisterStateCopyWithImpl<$Res, _$RegisteredImpl>
+    implements _$$RegisteredImplCopyWith<$Res> {
+  __$$RegisteredImplCopyWithImpl(
+      _$RegisteredImpl _value, $Res Function(_$RegisteredImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$RegisteredImpl implements _Registered {
+  const _$RegisteredImpl();
+
+  @override
+  String toString() {
+    return 'UserRegisterState.registered()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$RegisteredImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() registering,
+    required TResult Function() registered,
+    required TResult Function() errorOccured,
+  }) {
+    return registered();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? registering,
+    TResult? Function()? registered,
+    TResult? Function()? errorOccured,
+  }) {
+    return registered?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? registering,
+    TResult Function()? registered,
+    TResult Function()? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (registered != null) {
+      return registered();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_Registering value) registering,
+    required TResult Function(_Registered value) registered,
+    required TResult Function(_Error value) errorOccured,
+  }) {
+    return registered(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_Registering value)? registering,
+    TResult? Function(_Registered value)? registered,
+    TResult? Function(_Error value)? errorOccured,
+  }) {
+    return registered?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_Registering value)? registering,
+    TResult Function(_Registered value)? registered,
+    TResult Function(_Error value)? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (registered != null) {
+      return registered(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Registered implements UserRegisterState {
+  const factory _Registered() = _$RegisteredImpl;
+}
+
+/// @nodoc
+abstract class _$$ErrorImplCopyWith<$Res> {
+  factory _$$ErrorImplCopyWith(
+          _$ErrorImpl value, $Res Function(_$ErrorImpl) then) =
+      __$$ErrorImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$ErrorImplCopyWithImpl<$Res>
+    extends _$UserRegisterStateCopyWithImpl<$Res, _$ErrorImpl>
+    implements _$$ErrorImplCopyWith<$Res> {
+  __$$ErrorImplCopyWithImpl(
+      _$ErrorImpl _value, $Res Function(_$ErrorImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$ErrorImpl implements _Error {
+  const _$ErrorImpl();
+
+  @override
+  String toString() {
+    return 'UserRegisterState.errorOccured()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$ErrorImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() registering,
+    required TResult Function() registered,
+    required TResult Function() errorOccured,
+  }) {
+    return errorOccured();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? registering,
+    TResult? Function()? registered,
+    TResult? Function()? errorOccured,
+  }) {
+    return errorOccured?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? registering,
+    TResult Function()? registered,
+    TResult Function()? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (errorOccured != null) {
+      return errorOccured();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_Registering value) registering,
+    required TResult Function(_Registered value) registered,
+    required TResult Function(_Error value) errorOccured,
+  }) {
+    return errorOccured(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_Registering value)? registering,
+    TResult? Function(_Registered value)? registered,
+    TResult? Function(_Error value)? errorOccured,
+  }) {
+    return errorOccured?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_Registering value)? registering,
+    TResult Function(_Registered value)? registered,
+    TResult Function(_Error value)? errorOccured,
+    required TResult orElse(),
+  }) {
+    if (errorOccured != null) {
+      return errorOccured(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Error implements UserRegisterState {
+  const factory _Error() = _$ErrorImpl;
+}
